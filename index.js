2b31aa5420fa3d7ee36df3062f94374eec84b24a
jahedul-code <jahedul100200@gmail.com>
2026-02-27 00:27:40 +0600

    set the movement of cards & some html resolved

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let currentTab = "all";
 const tabActive = ["bg-blue-500", "text-white"];
-const tabInactive = ["bg-white", "text-gray-500"]
+const tabInactive = ["bg-white", "text-gray-500"];
+
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById("interview-container");
+const rejectedContainer = document.getElementById("rejected-container");
+
+
 
 function switchTab(tab) {
     
@@ -15,6 +21,21 @@ function switchTab(tab) {
         tabName.classList.add( ...tabInactive);
         tabName.classList.remove( ...tabActive);
     }
+    }
+
+    const pages = [allContainer, interviewContainer, rejectedContainer];
+    for (const div of pages) {
+        div.classList.add("hidden");
+        
+    }
+
+
+    if(tab === "all"){
+        allContainer.classList.remove("hidden");
+    }else if(tab === "interview"){
+        interviewContainer.classList.remove("hidden");
+    }else{
+        rejectedContainer.classList.remove("hidden");
     }
    
 }
